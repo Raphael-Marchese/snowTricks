63e291a594fa55a735ab7fc3b8858e1f70bab3e4
Raphael Marchese <marchese.raphael@gmail.com>
2024-11-07 16:11:52 +0100

    Create homepage controller, global website layout and start homepage content

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\FigureRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    public function __invoke(#[CurrentUser] ?User $user, FigureRepository $figureRepository): Response
+    {
+        $figures = $figureRepository->findAll();
+
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+            'figures' => $figures
+        ]);
+    }
+}
