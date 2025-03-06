08e452f973ea2fd21304154ae61235a765b09376
Raphael Marchese <marchese.raphael@gmail.com>
2025-03-06 11:48:33 +0100

    add category, image and videos entity and fix categories display

---
+++ src/Entity/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use App\Service\CategoryNameEnum;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    public ?int $id = null;
+
+    #[ORM\Column(type: Types::STRING, unique: true, enumType: CategoryNameEnum::class)]
+    public CategoryNameEnum $name;
+
+}
--- src/Entity/Figure.php
+++ src/Entity/Figure.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\FigureRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -26,21 +28,95 @@ class Figure
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     public ?string $description = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    public ?string $figureGroup = null;
-
-    #[ORM\Column(type: Types::JSON, nullable: true)]
-    public ?array $illustrations = null;
-
-    #[ORM\Column(type: Types::JSON, nullable: true)]
-    public ?array $videos = null;
+    #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'figures')]
+    #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')]
+    public ?Category $figureGroup = null;
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: false)]
     public ?User $author = null;
 
+    /**
+     * @var Collection<int, Image>
+     */
+    #[ORM\OneToMany(targetEntity: Image::class, mappedBy: 'figure')]
+    public Collection $illustrations;
+
+    /**
+     * @var Collection<int, Video>
+     */
+    #[ORM\OneToMany(targetEntity: Video::class, mappedBy: 'figure')]
+    public Collection $videos;
+
     public function __construct()
     {
         $this->createdAt = new \DateTimeImmutable();
+        $this->illustrations = new ArrayCollection();
+        $this->videos = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection<int, Image>
+     */
+    public function getIllustrations(): Collection
+    {
+        return $this->illustrations;
+    }
+
+    public function addIllustration(Image $illustration): static
+    {
+        if (!$this->illustrations->contains($illustration)) {
+            $this->illustrations->add($illustration);
+            $illustration->setFigure($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIllustration(Image $illustration): static
+    {
+        if ($this->illustrations->removeElement($illustration)) {
+            // set the owning side to null (unless already changed)
+            if ($illustration->getFigure() === $this) {
+                $illustration->setFigure(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Video>
+     */
+    public function getVideos(): Collection
+    {
+        return $this->videos;
+    }
+
+    public function addVideo(Video $video): static
+    {
+        if (!$this->videos->contains($video)) {
+            $this->videos->add($video);
+            $video->setFigure($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVideo(Video $video): static
+    {
+        if ($this->videos->removeElement($video)) {
+            // set the owning side to null (unless already changed)
+            if ($video->getFigure() === $this) {
+                $video->setFigure(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCategoryName(): string
+    {
+        return $this->figureGroup->name->value;
     }
 }
